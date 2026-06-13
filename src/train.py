1d96048a42e32f74af5553f820d639350ef30006
Osval Montesinos <osvalml@localhost.localdomain>
2026-06-12 18:23:52 -0600

    +Carpeta localizada src

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 from roboflow import Roboflow
 
-rf = Roboflow(api_key="IducOaHIU8ZFz1tf5ba2")
+rf = Roboflow(api_key="")
 project = rf.workspace("osval-elias-montesinos-valladares").project("annotation_public_dataset-ppyek")
 version = project.version(1)
 dataset = version.download("yolov8")
